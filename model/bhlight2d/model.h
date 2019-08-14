86cc5abc74cc8afa45ddabb32877d4dd9d78c05c
Ben Prather <bprathr2@illinois.edu>
2019-08-14 17:28:41 -0500

    Oops, forgot models due to gitignore. Fixed & updated .gitignore

---
+++ model/bhlight2d/model.h
@@ -0,0 +1,19 @@
+#define NX (32)
+#define NY (32)
+
+#define KRHO      (0)
+#define UU        (1)
+#define U1        (2)
+#define U2        (3)
+#define U3        (4)
+#define B1        (5)
+#define B2        (6)
+#define B3        (7)
+#define KTOT      (8)
+#define KELCOND   (9)
+#define KELNOCOND (10)
+#define PHI       (11)
+#define FLR       (12)
+#define NPRIM     (13)
+
+void init_physical_quantities(void);
