19bd54a65f36a189b40e143d0de5f06df7e3e0e7
Ben Prather <bprathr2@illinois.edu>
2019-09-18 14:31:37 -0500

    Revise test script to compile problem.  Specify new parameters in tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -7,7 +7,12 @@ do
   folder=${folder: : -1}
   if [[ "$folder" != "test-resources" ]]; then
     cd $folder
-    #../../ipole -par $folder.par &> out_$folder.txt
+    if [[ $folder == *"sample_dump"* ]]; then
+      make -f ../../makefile -j8 MODEL=iharm
+    else
+      make -f ../../makefile -j8 MODEL=thin_disk
+    fi
+    ./ipole -par $folder.par &> out_$folder.txt
     h5diff -d 1.e-10 image.h5 ../test-resources/$folder.h5
     h5diff -p 1.e-6 image.h5 ../test-resources/$folder.h5
     cd -
