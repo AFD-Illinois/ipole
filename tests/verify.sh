1f2f20285df6e595854474eb72417abc35529356
Ben Prather <bprathr2@illinois.edu>
2020-04-27 10:02:22 -0500

    First stab at CI script

--- tests/verify.sh
+++ tests/verify.sh
@@ -5,9 +5,11 @@
 
 folder=$(basename $PWD)
 
+# Note these return nonzero exit code for most test runs...
 echo "Pixels different by >1% absolute"
 h5diff -d 1.e-2 ../test-resources/${folder}_image.h5 image.h5 /pol
 echo "Pixels different by >1% relative"
 h5diff -p 1.e-2 ../test-resources/${folder}_image.h5 iamge.h5 /pol
 
+# ...but we should only fail if this one returns nonzero
 python ../../scripts/compare.py ../test-resources/${folder}_image.h5 image.h5
