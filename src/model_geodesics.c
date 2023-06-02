4081a0d66d898bf7d1bce75e97b08ed134e8345f
George N. Wong <gnwong@ias.edu>
2023-06-02 14:59:21 -0400

    update ipole to support cartesian z turning points for tracking subrings

--- src/model_geodesics.c
+++ src/model_geodesics.c
@@ -142,9 +142,22 @@ int trace_geodesic(double Xi[NDIM], double Kconi[NDIM], struct of_traj *traj, do
     } else {
       // ignore first few steps to deal with ever-changing initial condition
       if (nstep > 3) {
+        // check for turning points in z for geodesics that will
+        // strike midplane at infinity
+        double ra, rb, rc;
+        double ha, hb, hc;
+        bl_coord(traj[nstep-2].X, &ra, &ha);
+        bl_coord(traj[nstep-1].X, &rb, &hb);
+        bl_coord(traj[nstep-0].X, &rc, &hc);
+        double dz_1 = rb * cos(hb) - ra * cos(ha);
+        double dz_2 = rc * cos(hc) - rb * cos(hb);
+        if (dz_1 * dz_2 < 0) nturns++;
+        /*
+        // legacy support for turning points in theta
         double dX2a = traj[nstep-1].X[2] - traj[nstep-2].X[2];
         double dX2b = traj[nstep].X[2] - traj[nstep-1].X[2];
         if (dX2a * dX2b < 0) nturns++;
+         */
       }
     }
 
