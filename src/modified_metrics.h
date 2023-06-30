363a51883cea608f54ca4fb4bce08c506e70b262
Tegan Thomas <tt29@bh28.astro.illinois.edu>
2023-06-30 11:41:37 -0500

    added alt metric file

---
+++ src/modified_metrics.h
@@ -0,0 +1,9 @@
+#ifndef MODIFIED_METRICS_H
+#define MODIFIED_METRICS_H
+
+#include "decs.h"
+
+void gcov_EdGB_ks(double r, double th, double zeta, double gcov[NDIM][NDIM]);
+void gcov_DCS_ks(double r, double th, double zeta, double gcov[NDIM][NDIM]);
+
+#endif
